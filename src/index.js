e9f2179e75c18345d3e98e2d96b6e1baf4644179
Andon Zeno <tonizeno5@gmail.com>
2024-09-21 21:53:06 +0200

    feat: serverless setup with express and neon postgres

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const { neon } = require("@neondatabase/serverless")
+
+const app = express();
+
+async function dbClient() {
+  const sql = neon(process.env.DATABASE_URL);
+
+  return sql
+}
+
+app.get("/", async (req, res, next) => {
+  const db = await dbClient();
+  const results = await db`SELECT NOW()`;
+
+
+  return res.status(200).json({
+    message: "Hello from root!",
+    results
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+exports.handler = serverless(app);
